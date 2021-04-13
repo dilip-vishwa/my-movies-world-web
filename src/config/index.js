d484773ba16f54e032b310a1282a89ef8c7c8a04
Dilip Vishwakarma <dilipslv7@gmail.com>
2021-04-14 02:01:21 +0530

    Made some changes

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,7 @@ import dev_endpoint from './development';
 import prod_endpoint from './production';
 
 
-const env = process.env.NODE_ENV1 || 'development';
+const env = process.env.NODE_ENV || 'development';
 
 let endpoint = env == "production" ? prod_endpoint : dev_endpoint
 export default {
